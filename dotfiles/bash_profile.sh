2de15b767b384b0835f6c6e6780e4f76e0a78b2d
Shayne Trosdahl <shayne@trozdol.com>
2018-04-30 17:29:34 -0500

    pre mac wipe

--- dotfiles/bash_profile.sh
+++ dotfiles/bash_profile.sh
@@ -39,4 +39,4 @@ complete -W "NSGlobalDomain" defaults;
 #
 complete -o "nospace" -W "Contacts Calendar Dock Finder Mail Safari iTunes SystemUIServer Terminal Twitter" killall;
 
-export PATH="/Users/trozdol/.bin/Sencha/Cmd:$PATH"
+export PATH="$HOME/.bin/Sencha/Cmd:$PATH"
--- dotfiles/mount-nfs.sh
+++ dotfiles/mount-nfs.sh
@@ -3,5 +3,5 @@
 # Mount NFS Volumes
 sudo mount_nfs -o resvport,rw 10.1.10.200:/home/nfs/nfsshare ~/10.1.10.200/nfs &&
 sudo mount_nfs -o resvport,rw 10.1.10.200:/home/nfs/vm       ~/10.1.10.200/vms &&
-sudo mount_nfs -o resvport,rw 10.1.10.200:/home/nfs/depot-01 ~/10.1.10.200/depot
-#sudo mount_nfs -o resvport,rw 
+sudo mount_nfs -o resvport,rw 10.1.10.200:/home/nfs/depot-01 ~/10.1.10.200/depot &&
+sudo mount_nfs -o resvport,rw 10.1.10.200:/var/nfsshare      ~/10.1.10.200/nfsshare
